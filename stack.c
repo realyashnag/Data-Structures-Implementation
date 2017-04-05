5be3d4c9bfc27e98b080c524f4a616febf642d31
Yash Nag <hsayganalpha@gmail.com>
2017-04-06 02:03:37 +0530

    Create stack.c
    Stack Implementation using C

---
+++ stack.c
@@ -0,0 +1,42 @@
+void create_stack(stack *s, int size)
+{
+    s->arr = (int*)malloc(sizeof(int)*size);
+    s->size = size;
+    s->top = -1;
+}
+
+int pop(stack* s)
+{
+    if (s->top < 0)
+    {
+        printf("\nError : Stack is empty!");
+        return -1;
+    }
+    else
+    {
+        return s->arr[s->top--];
+    }
+}
+
+void push(stack *s, int var)
+{
+    if (s->top+1 >= s->size)
+    {
+        printf("\nError : Stack is full!");
+    }
+    else
+    {
+        s->arr[++s->top] = var;
+    }
+}
+
+void print_stack(stack *s)
+{
+    int i=0;
+    printf("\nStack : \n");
+    for (i=s->top; i>=0; i--)
+    {
+        printf("|%d|\n", s->arr[i]);
+    }
+    printf("|_|\n");
+}
